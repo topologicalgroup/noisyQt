9a330648a40bcfe32a100690250d0900c10d9cc1
iya5 <175221759+iya5@users.noreply.github.com>
2026-06-01 23:47:52 +0800

    setting up UI

--- src/ui.py
+++ src/ui.py
@@ -1,1 +1,27 @@
 # file for the ui
+import sys
+
+from PySide6 import QtCore
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout,
+    QPushButton, QFileDialog, QSizePolicy, QMessageBox, QScrollArea,
+)
+
+
+class denoiseApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Image Denoiser")
+
+        self.text = QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+
+def main():
+    app = QApplication(sys.argv)
+    w = denoiseApp(); w.resize(1100, 700); w.show()
+    sys.exit(app.exec())
+
+main()
