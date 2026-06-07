bc59ee35f3715db4d910d635a4251976dca10745
iya5 <175221759+iya5@users.noreply.github.com>
2026-06-07 17:15:28 +0800

    created load image func, and displays image file path

--- src/ui.py
+++ src/ui.py
@@ -1,7 +1,9 @@
 # file for the ui
 import sys
 
-from PySide6 import QtCore
+from pathlib import Path
+from PySide6 import QtCore 
+from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout,
     QPushButton, QFileDialog, QSizePolicy, QMessageBox, QScrollArea,
@@ -14,10 +16,55 @@ class denoiseApp(QWidget):
 
         self.setWindowTitle("Image Denoiser")
 
-        self.text = QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+        """# UI elements
+        load_btn = QPushButton("Load Image")
+        self.image_label = QLabel("No image loaded")
 
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.text)
+        # Layout
+        btn_row = QHBoxLayout()
+        btn_row.addWidget(load_btn)
+
+        lbl_row = QHBoxLayout()
+        lbl_row.addWidget(self.image_label)
+
+        main = QVBoxLayout(self)
+        main.addLayout(btn_row)
+        main.addLayout(lbl_row)
+
+        # Signals
+        load_btn.clicked.connect(self.load_image)
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)"""
+
+        # Widgets
+        load_btn = QPushButton("Load Image")
+
+        self.image_label = QLabel("No image loaded")
+        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Layouts
+        btn_row = QHBoxLayout()
+        btn_row.addWidget(load_btn)
+
+        lbl_row = QHBoxLayout()
+        lbl_row.addWidget(self.image_label)
+
+        main = QVBoxLayout(self)
+
+        main.addLayout(btn_row)
+        main.addLayout(lbl_row)
+
+        # Signals
+        load_btn.clicked.connect(self.load_image)
+
+    def load_image(self):
+        path, _ = QFileDialog.getOpenFileName(
+            self,
+            "Open Image",
+            "",
+            "Images (*.png *.jpg *.jpeg *.bmp *.tif *.tiff)"
+        )
+
+        self.image_label.setText(path)
 
 def main():
     app = QApplication(sys.argv)
